cf5549323a47dec166f7fb1c5c743da7da2b9109
Elisha Sunday <sundayelisha179@gmail.com>
2026-02-24 11:27:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9199109671-696d1",
+  "appId": "1:88159310423:web:1ddbd9163f548d8322c912",
+  "apiKey": "AIzaSyBe_e9Yd6b-1L2CH3jEJmQGc0vznR7Q59E",
+  "authDomain": "studio-9199109671-696d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88159310423"
+};
